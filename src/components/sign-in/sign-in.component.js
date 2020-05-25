ac5ade26beb673d9a347a595a3cc9a5b31050f6e
Jayden Shaw <jayden.shaw@gmail.com>
2020-05-25 14:29:34 -0600

    Created styling for form inputs.

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import FormInput from '../form-input/form-input.component'
+
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component { 
@@ -9,20 +11,20 @@ class SignIn extends React.Component {
         this.state = {
             email: '',
             password: ''
-        }
+        };
     }
 
     handleSubmit = event => {
         event.preventDefault();
 
         this.setState({ email: '', password: ''})
-    }
+    };
 
     handleChange = event => {
         const { value, name } = event.target;
 
         this.setState({ [name]: value })
-    }
+    };
 
     render() {
         return(
@@ -31,25 +33,26 @@ class SignIn extends React.Component {
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <input 
-                        name='email' 
+                    <FormInput 
+                        name='email'
+                        type='email'
+                        handleChange={this.handleChange}
                         value={this.state.email} 
+                        label='email'
                         required 
                     />
-                    <label>Email</label>
-                    <input 
+                    <FormInput 
                         name='password' 
                         type='password'
-                        onChange={this.handleChange} 
+                        handleChange={this.handleChange} 
                         value={this.state.password} 
+                        label='password'
                         required 
                     />
-                    <label>Password</label>
-
                     <input type='submit' value='Submit Form' />
                 </form>
             </div>
-        )
+        );
     }
 }
 
