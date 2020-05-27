50596b66daeaf8597006d446aca9a1fd3c889088
Jayden Shaw <jayden.shaw@gmail.com>
2020-05-27 11:42:53 -0600

    Resolved issue with Sign in with Google requesting on site form to be filled out

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -63,7 +63,7 @@ class SignIn extends React.Component {
                         <CustomButton type='submit'>
                             SIGN IN
                         </CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                        <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn>
                             {' '}SIGN IN WITH GOOGLE{' '}
                         </CustomButton>
                     </div>
